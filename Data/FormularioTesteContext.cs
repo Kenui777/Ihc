7cf21cec25a06b0f737736f8523a6357ab2909d3
Fábio Florencio <devops@safeit.com.br>
2024-10-21 13:25:42 -0300

    Adicionar arquivos de projeto.

---
+++ Data/FormularioTesteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Validacao_Form.Models;
+
+namespace FormularioTeste.Data
+{
+    public class FormularioTesteContext : DbContext
+    {
+        public FormularioTesteContext (DbContextOptions<FormularioTesteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Validacao_Form.Models.Denuncia> Denuncia { get; set; } = default!;
+    }
+}
